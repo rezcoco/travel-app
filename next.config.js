1e0fdce52752e0bc733f790cf48399080c3e87af
rezcoco <rezaainulzaki23@gmail.com>
2023-12-18 23:23:09 +0700

    nothing

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   poweredByHeader: false,
+  experimental: {
+    serverComponentsExternalPackages: ["@react-email/components",
+      "@react-email/tailwind"]
+  }
 }
 module.exports = nextConfig
